839d4ace2f97ac61720fc0eb9e421c0c720dc61b
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-01-29 21:30:39 +0100

    Add PlayerController.cs

--- Assets/CSR/Scripts/PlayerController.cs
+++ Assets/CSR/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody myRigidbody;
+    public float moveSpeed = 5f;
+    public float jumpForce = 500f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Preferably get input in Update()
+        var moveInput = Input.GetAxis("Horizontal");
+
+        //Preferably interact with physics in FixedUpdate()
+        myRigidbody.velocity = new Vector3(moveInput * moveSpeed, myRigidbody.velocity.y, 0);
+
+
+        //Get jump input
+        //Preferably get input in Update()
+        var jumpInput = Input.GetKeyDown(KeyCode.Space);
+
+        //Apply jump force
+        //Preferably interact physics in FixedUpdate()
+        if (jumpInput && myRigidbody.velocity.y == 0)
+            myRigidbody.AddForce(Vector3.up * jumpForce);
+
     }
 }
