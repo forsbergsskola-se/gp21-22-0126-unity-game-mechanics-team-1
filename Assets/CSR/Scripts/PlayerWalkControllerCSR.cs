57255afe422988b74a48dbecc2efbd4a22863585
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-01-30 14:22:36 +0100

    AddPlayerInputController

--- Assets/CSR/Scripts/PlayerWalkControllerCSR.cs
+++ Assets/CSR/Scripts/PlayerWalkControllerCSR.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,16 @@ public class PlayerWalkControllerCSR : MonoBehaviour
 {
     public Rigidbody myRigidbody;
     public float moveSpeed = 5f;
+    public PlayerInputControllerCSR playerInputControllerCsr;
+
 
     private void Update()
     {
+        //Get move input
         //Preferably get input in Update()
         var moveInput = Input.GetAxis("Horizontal");
 
+        //Set move velocity
         //Preferably interact with physics in FixedUpdate()
         myRigidbody.velocity = new Vector3(moveInput * moveSpeed, myRigidbody.velocity.y, 0);
     }
