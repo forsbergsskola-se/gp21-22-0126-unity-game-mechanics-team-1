a8e0e8b77de74a95d0df83f4775f08bf86ed9be6
Muhammed Al <muhammed.al-idani@student.forsbergsskola.se>
2022-01-31 14:43:26 +0100

    dash system

--- Assets/Mohammed Scene/Scripts/PlayerControllerMA.cs
+++ Assets/Mohammed Scene/Scripts/PlayerControllerMA.cs
@@ -1,17 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerControllerMA : MonoBehaviour
 {
-    public float moveInput;
-    public Rigidbody myRigidbody;
-    public float speed;
+    [SerializeField] private float moveInput;
+   [SerializeField] Rigidbody myRigidbody;
+   [SerializeField] private float speed;
 
-    
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private  float dashTimeCounter = 0f;
+
+    private bool canDash;
+    private bool canWalk;
+
+    private void Start()
+    {
+        canDash = true;
+        canWalk = true;
+    }
+
     void Update()
     {
         moveInput = Input.GetAxis("Horizontal");
-        myRigidbody.velocity = new Vector3(speed * moveInput, myRigidbody.velocity.y);
+        if (Input.GetKeyDown(KeyCode.W) && canDash && moveInput != 0 )
+        {
+            canDash = false;
+            canWalk = false;
+            var inputNormalized = Mathf.Sign(moveInput);
+            StartCoroutine(dash(inputNormalized));
+            
+        }
+        else if(canWalk)
+        {
+            
+            myRigidbody.velocity = new Vector3(speed * moveInput, myRigidbody.velocity.y);  
+        }
+    }
+    
+    private IEnumerator dash(float input)
+    {
+        while (dashTimeCounter <= dashTime )
+        {
+            myRigidbody.velocity = new Vector3(dashSpeed * input, myRigidbody.velocity.y);
+            dashTimeCounter += Time.deltaTime;
+            yield return null;
+        }
+        myRigidbody.velocity = Vector3.zero;
+    
+        canDash = true;
+        canWalk = true;
+        dashTimeCounter = 0f;
+        Debug.Log(canDash);
+        yield return null;
     }
 }
