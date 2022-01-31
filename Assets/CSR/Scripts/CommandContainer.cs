26974dd4f62e404a4a95718bf02b72f4def2e47c
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-01-31 20:21:14 +0100

    CreateCommandContainer

---
+++ Assets/CSR/Scripts/CommandContainer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandContainer : MonoBehaviour
+{
+    public float wolkCommand;
+    public bool JumpCommandDowen;
+    public bool JumpCommandUp;
+    public bool JumpICommand;
+}
