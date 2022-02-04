1251a3f1934ffb95809feb1294d1835e7d34dfe3
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-02-04 11:38:54 +0100

    Enemy Explodes

--- Assets/Scripts/Sokratis/AI/ExplosiveEnemyAI.cs
+++ Assets/Scripts/Sokratis/AI/ExplosiveEnemyAI.cs
@@ -1,12 +1,11 @@
-using System;
 using UnityEngine;
 
 public class ExplosiveEnemyAI : MonoBehaviour
 {
     [Header("Dependencies")]
     [SerializeField] private Rigidbody rigidbody;
-
     [SerializeField] private Transform target;
+    [SerializeField] private Explosion_1_Timer explosion;
     
     [Header("Stats")] 
     [SerializeField] private float range;
@@ -20,6 +19,9 @@ public class ExplosiveEnemyAI : MonoBehaviour
     private void FixedUpdate()
     {
         var direction = target.transform.position - transform.position;
+        
+        if(direction.magnitude <= explosion.Radius) explosion.Trigger();
+        
         Physics.SphereCast(transform.position, 1, direction, out RaycastHit hit, range);
 
         if (hit.transform == null || !hit.transform.gameObject.CompareTag("Player")) return;
