4f3bd6b9a847aa8d280d012b39c69de0b6be6241
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-02-07 14:15:16 +0100

    Dash Implemented

---
+++ Assets/Scripts/Mohammed/Dash/Dash_Imidiate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class Dash_Imidiate
+{
+    private Rigidbody _rigidbody;
+    private float _speed;
+    private float _time;
+
+    private bool canWalk = true;
+    private bool canDash = true;
+    
+    public Dash_Imidiate(Rigidbody rb, float speed, float time)
+    {
+        _rigidbody = rb;
+        _speed = speed;
+        _time = time;
+    }
+
+    public IEnumerator Execute(float input)
+    {
+        float timeCounter = 0f;
+        canDash = false;
+        canWalk = false;
+        
+        while (timeCounter <= _time)
+        {
+            _rigidbody.velocity = new Vector3(_speed * input, _rigidbody.velocity.y);
+            timeCounter += Time.deltaTime;
+            yield return null;
+        }
+
+        _rigidbody.velocity = Vector3.zero;
+
+        canDash = true;
+        canWalk = true;
+        timeCounter = 0f;
+
+        yield return null;
+    }
+
+    public bool CanWalk() => canWalk;
+    public bool CanDash() => canWalk;
+}
--- Assets/Scripts/_FromClasses/PlayerWalkController.cs
+++ Assets/Scripts/_FromClasses/PlayerWalkController.cs
@@ -1,21 +1,33 @@
-using System;
 using UnityEngine;
 
 public class PlayerWalkController : MonoBehaviour
 {
+    [Header("Dependencies")]
     [SerializeField] private Rigidbody myRigidbody;
     [SerializeField] private PlayerInputController playerInputController;
     [SerializeField] private GroundChecker groundChecker;
+    
+    [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float chargingMoveSpeedFactor = 0.5f;
 
+    [Header("Dash")] 
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+
     private Walk _walk;
+    private Dash_Imidiate _dash;
+    
     public bool CanWalk { get; set; }
+    public bool CanDash { get; set; }
 
     private void Awake()
     {
         _walk = new Walk(myRigidbody, moveSpeed);
+        _dash = new Dash_Imidiate(myRigidbody, dashSpeed, dashTime);
+        
         CanWalk = true;
+        CanDash = true;
     } 
     
     private void Update()
@@ -24,8 +36,20 @@ public class PlayerWalkController : MonoBehaviour
         var currentMoveSpeed = moveSpeed;
         if (playerInputController.JumpInput && groundChecker.IsGrounded)
             currentMoveSpeed *= chargingMoveSpeedFactor;
+       
 
-        if(CanWalk) _walk.Move(playerInputController.MoveInput);
+        if (playerInputController.DashInput && CanDash && playerInputController.MoveInput != 0)
+        {
+            Debug.Log("W kaye and MoveInput");
+            CanDash = false;
+            CanWalk = false;
+            var inputNormalized = Mathf.Sign(playerInputController.MoveInput);
+            StartCoroutine(_dash.Execute(inputNormalized));
+        }
+        else if(CanWalk) _walk.Move(playerInputController.MoveInput);
+        
+        CanWalk = _dash.CanWalk();
+        CanDash = _dash.CanDash();
     }
 
     private void OnCollisionEnter(Collision other) => CanWalk = true;
