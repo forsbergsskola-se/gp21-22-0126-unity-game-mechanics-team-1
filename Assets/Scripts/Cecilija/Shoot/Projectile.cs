c0366cc0d9491683258d3558b74cc58741f6e052
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-02-09 16:03:33 +0100

    Update Projectile.cs

--- Assets/Scripts/Cecilija/Shoot/Projectile.cs
+++ Assets/Scripts/Cecilija/Shoot/Projectile.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidbody;
+    
     [SerializeField] private float speed;
+    
     [SerializeField] Vector3 shootDirection;
-
+    
     private void Start()
     {
         rigidbody.velocity = shootDirection * speed;
     }
-
-    private void OnCollisionEnter(Collision other) => Destroy(gameObject);
     
+    private void OnCollisionEnter(Collision other)
+    { 
+        if(!other.gameObject.CompareTag("Player")) Destroy(gameObject);
+    }
 }
